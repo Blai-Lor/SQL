c333acf4c10b2493102ce49f9504ec1b8645367a
Blai Lorente <lorentefusterblai@gmail.com>
2026-04-10 14:55:39 +0200

    Update Associative-Arrays.sql

--- Unidad 8 PLSQL/Associative-Arrays.sql
+++ Unidad 8 PLSQL/Associative-Arrays.sql
@@ -3,8 +3,7 @@ DECLARE
     v_sales sales;
 
 BEGIN
-    SELECT NVL(COUNT(order_id,0) FROM ORDERS order LEFT JOIN r.EMPLOYERS em ON em.employers_id = order WHERE or.status='Shiped' AND 
-    em.first);
+    SELECT NVL(COUNT(order_id,0) FROM ORDERS order LEFT JOIN r.EMPLOYERS em ON em.employers_id = order WHERE or.status='Shiped' AND em.first);
     SELECT COUNT(o.order_id) INTO v_sales('Blake') FROM orders o
     JOIN employees e ON e.employee_id = o.salesman_id WHERE o.status = 'Shipped' AND e.first_name = 'Blake';
     SELECT COUNT(o.order_id) INTO v_sales('Daisy') FROM orders o
